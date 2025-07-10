c1df3aa15eb094e5b7ded2b0b91bbe24885d2943
clintonobru <obruclinton@gmail.com>
2025-07-10 18:04:44 +0100

    Seventh commit

--- my-app/src/component/Home.jsx
+++ my-app/src/component/Home.jsx
@@ -1,5 +1,6 @@
 // import ThemedButton from "../context/ThemedButton";
 import Form from "./Form";
+import { Link } from "react-router-dom"
 
 function Home() {
     return (
@@ -8,7 +9,10 @@ function Home() {
                 <h1 className="home_h1">Welcome To Home Page</h1>
                 <Form />
             </div>
-            
+            <ul>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/dashboard">Dashboard</Link></li>
+            </ul>
         </>
     );
 }
